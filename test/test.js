8953701fd97b3a6b1426149bcd622764597fb9af
Josh Duff <me@JoshDuff.com>
2015-11-23 11:02:24 -0600

    Bumping linkifier dependency
    And testing that no error happens with [[s in code blocks

--- test/test.js
+++ test/test.js
@@ -142,3 +142,21 @@ test('escaping characters it shouldn\'t when converting to markdown', function(t
 	})
 
 })
+
+test('a post on noddity.com with a link in a code block', function(t) {
+	var state = makeTestState()
+
+	state.retrieval.addPost('post', { title: 'TEMPLAAAATE' }, '{{>current}}')
+	state.retrieval.addPost('the-most-boring-page.md', {}, 'boring')
+
+	var metadata = {
+		title: 'Post/template documentation',
+		date: new Date('Thu Jun 11 2015 20:16:56 GMT-0500 (CDT)')
+	}
+	state.retrieval.addPost('test', metadata, 'Posts can be [straight markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet), but there are some other goodies.  To start with, you can use goodies like footnotes and tables that are supported by the [remarkable](https://www.npmjs.com/package/remarkable#syntax-extensions) parser.\n\n## Internal links\n\nYou can link to any other document on the site using a syntax similar to Wikipedia\'s: anything inside square brackets will turn into a link.  `"[[noddity-backend.md]]"` turns into "[[noddity-backend.md]]".\n\nYou can put in whatever link text you like after a `|` pipe: `[[noddity-backend.md|CLICK THIS LINK]]` turns into [[noddity-backend.md|CLICK THIS LINK]].\n\n## Embeddable templates\n\nAny page on the site can be embedded into any other page.  Take [[the-most-boring-page.md|this boring page]] for example - you can visit it by clicking it on the link, but you can also embed it by including its name inside of colons, like this: `::the-most-boring-page.md::`.\n\nWhen I do that in this page, you get: ::the-most-boring-page.md::\n\nIf you want your templates to not be parsed with the markdown parser (which adds paragraph tags, among other things) you can add the `markdown: false` property to the metadata at the top of the file.\n\n### Expressions\n\nInside templates, you can also use fancy expressions - they get inserted as [Ractive templates](http://docs.ractivejs.org/latest/mustaches), which means you can pretty much use regular JavaScript inside moustaches.\n\nInside those expressions, you have certain values available to you.  You have the parameters passed in to the template, either as numbered expressions: `::template.md|value 1|value 2::` or as named expressions: `::template.md|first=value 1|first=value 2::`.\n\nAlso available to you are:\n\n- `current` - the file name of the page you are current on.  Set by the noddity-renderer\n- `postList` - changing soon\n- all the values set in your config.js')
+
+	state.render('post', 'test', {}, function(err, html) {
+		t.notOk(err)
+		t.end()
+	})
+})
